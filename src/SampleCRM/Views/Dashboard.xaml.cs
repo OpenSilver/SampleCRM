57c2e940ada57e8b86e1561b58d01e90dafa5580
oguzkoroglu <oguzkoroglu@gmail.com>
2023-12-31 20:28:00 +0300

    Old busy indicator has been removed
    Old busy indicator has been removed

--- src/SampleCRM/Views/Dashboard.xaml.cs
+++ src/SampleCRM/Views/Dashboard.xaml.cs
@@ -1,5 +1,4 @@
-﻿using OpenRiaServices.DomainServices.Client;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows;
