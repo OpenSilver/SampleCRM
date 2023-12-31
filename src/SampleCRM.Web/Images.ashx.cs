38df679c5dab6ed469e17656818513d21761ee22
oguzkoroglu <oguzkoroglu@gmail.com>
2023-12-31 06:57:38 +0300

    Simplyfied and Optimized GetProductImage function

--- src/SampleCRM.Web/Images.ashx.cs
+++ src/SampleCRM.Web/Images.ashx.cs
@@ -23,63 +23,38 @@ namespace SampleCRM.Web
 
         private static void GetProductImage(HttpContext context)
         {
-            var productId = context.Request.QueryString["productid"];
-            if (string.IsNullOrEmpty(productId))
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                context.Response.End();
-                return;
-            }
-
-            var productService = new ProductsService();
             try
             {
+                var productId = context.Request.QueryString["productid"];
+                if (string.IsNullOrEmpty(productId))
+                    throw new MissingFieldException("productid required");
+
+                var productService = new ProductsService();
                 var pict = productService.GetProductPicture(productId);
-                //context.Response.ContentType = "image/jpeg";
-                //context.Response.BinaryWrite(pict);
-                //context.Response.StatusCode = (int)HttpStatusCode.OK;
-
-                //clear the buffer stream
-                context.Response.ClearHeaders();
-                context.Response.Clear();
-                context.Response.Buffer = true;
-
-                //set the correct ContentType
                 context.Response.ContentType = "image/jpeg";
-
-                //set the filename for the image
-                context.Response.AddHeader("Content-Disposition", "attachment; filename=\"ImageName.bmp\"");
-
-                //set the correct length of the string being send
-                context.Response.AddHeader("content-Length", pict.Length.ToString());
-
-                //send the byte array to the browser
-                context.Response.OutputStream.Write(pict, 0, pict.Length);
-
-                //cleanup
-                context.Response.Flush();
-                context.ApplicationInstance.CompleteRequest();
+                context.Response.BinaryWrite(pict);
+            }
+            catch (MissingFieldException me)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Response.Status = me.Message;
             }
             catch (ArgumentNullException ea)
             {
                 context.Response.StatusCode = (int)HttpStatusCode.NotFound;
                 context.Response.Status = ea.Message;
-                context.Response.End();
             }
             catch (Exception e)
             {
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                 context.Response.Status = e.Message;
+            }
+            finally
+            {
                 context.Response.End();
             }
         }
 
-        public bool IsReusable
-        {
-            get
-            {
-                return false;
-            }
-        }
+        public bool IsReusable => true;
     }
 }
