761c19d9249188f0c3753fcfc9b12f5acd3e229e
Sylvain Maillard <maillard.sy@gmail.com>
2024-02-27 15:11:54 +0100

    Update reference to OpenSilver.AI.Client + use ConversationalUI

--- src/SampleCRM/Views/Assistant.xaml.cs
+++ src/SampleCRM/Views/Assistant.xaml.cs
@@ -21,7 +21,6 @@ namespace SampleCRM.Web.Views
 {
     public partial class Assistant : BasePage
     {
-        int _count = 0;
 
         public Assistant()
         {
@@ -36,94 +35,13 @@ namespace SampleCRM.Web.Views
                 TextWrapping = TextWrapping.Wrap
             };
 #else
-            InitializeAIAssistant();
+            MyConversationalUI.RiaGeneratedMetadata = new RiaGeneratedMetadata()
+            {
+                AssemblyContainingCode = Assembly.GetExecutingAssembly(),
+                DomainContextType = typeof(CustomersContext),
+                NamespaceOfEntitiesModel = "SampleCRM.Web.Models"
+            };
 #endif
         }
-
-
-        #region AI-related stuff
-#if USEAI
-
-        ObservableCollection<ChatMessage> _messages;
-        AIAssistant _assistant;
-        Action _isAlive;
-        AIChatControl AIChat;
-
-        BusyIndicator busyIndicatorForAICall;
-        BusyIndicator busyIndicatorForCodeExecution;
-
-        void InitializeAIAssistant()
-        {
-            //Programmatically creating the AIChatControl so the CRMSample that does not use AI can still build.
-            AIChat = new AIChatControl() { Header = "Assistant", Width=250, FontSize=22 };
-            AIChatControlContainer.Children.Add(AIChat);
-
-            busyIndicatorForAICall = new BusyIndicator();
-            busyIndicatorForCodeExecution = new BusyIndicator();
-            busyIndicatorForAICall.Content = busyIndicatorForCodeExecution;
-
-            _assistant = new AIAssistant
-                (
-                     placeWhereToDisplayGeneratedUI: PlaceWhereGeneratedUIIsShown,
-                     assemblyContainingRIAGeneratedCode: Assembly.GetExecutingAssembly(),
-                     namespaceOfEntitiesModel: "SampleCRM.Web.Models",
-                     busyIndicatorForAICall: busyIndicatorForAICall,
-                     busyIndicatorForCodeExecution: busyIndicatorForCodeExecution,
-                     domainContextType: typeof(CustomersContext)
-                );
-            LogMessageQueueListBox.ItemsSource = _assistant.LogMessageQueue;
-
-
-            _assistant.AIMessage += Assistant_AIMessage;
-
-            _isAlive = () => { _count++; ShowHideDebugPanel.Content = $"Debug ({_count})"; };
-
-            _messages = AIChat.ChatMessages;
-
-            AIChat.UserMessage += AiChatControl_UserMessage;
-            AIChat.QueryCancel += AiChatControl_QueryCancel;
-        }
-
-
-        private void Assistant_AIMessage(object sender, AIMessageEventArgs e)
-        {
-            if (e.Type == AIMessageType.Error)
-            {
-                _messages.Add(new ChatMessage(Owner.AIError, e.MessageText));
-            }
-            else
-            {
-                _messages.Add(new ChatMessage(Owner.AI, e.MessageText));
-            }
-        }
-
-        private void AiChatControl_QueryCancel(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private async void AiChatControl_UserMessage(object sender, UserMessageEventArgs e)
-        {
-            string message = e.MessageText;
-            if (!string.IsNullOrWhiteSpace(message))
-            {
-                ChatMessage acknowledgement = new ChatMessage(Owner.AI, "Please wait while I Process your request...");
-                _messages.Add(acknowledgement);
-                ChatMessage chatMessage = new ChatMessage(Owner.None, busyIndicatorForAICall);
-                _messages.Add(chatMessage);
-                await _assistant.Start(message, _isAlive);
-                _messages.Remove(chatMessage);
-                _messages.Remove(acknowledgement);
-            }
-        }
-
-        private async void ButtonRunAssistant_Click(object sender, RoutedEventArgs e)
-        {
-
-
-        }
-#endif
-
-        #endregion
     }
 }
